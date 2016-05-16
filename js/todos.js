2b102545f9b33ee3560ce1fe0608438814bdd710
Sebastian Neumann <Sebastian Neumann>
2016-05-16 22:50:42 +0200

    MondayHomeWork

--- js/todos.js
+++ js/todos.js
@@ -1,32 +1,56 @@
-$(document).ready(
+$(document)
+		.ready(
 
-		function() {
-			$("#datepicker").datepicker();
+				function() {
+					$("#datepicker").datepicker();
+					$('#inItemText').focus();
+					var totalItems = 0;
+					function addNewItem(cbId, itemText, deadline) {
+						totalItems++;
+						cbId = "";
+						itemText = $('#inItemText').val();
+						deadline = $('#datepicker').val();
+						$('#inItemText').focus();
+						if (!itemText || itemText == "" || itemText == " ") {
+							return false;
+						} else {
 
-			$('#inItemText').focus();
-			function addNewItem(cbId, itemText, deadline) {
-				cbId = "";
-				itemText = $('#inItemText').val();
-				deadline = $('#datepicker').val();
-				$('#inItemText').focus();
-				if (!itemText || itemText == "" || itemText == " ") {
-					return false;
-				}
+							$('#todoList')
+									.append(
+											'<li><input class="chkBox" type="checkbox" onclick="updateItemStatus(this)" id="cb_'
+													+ totalItems
+													+ '"/><span id="item_'
+													+ totalItems
+													+ '">'
+													+ itemText + '</span></li>');
+							$('#deadlineList').append(
+									'<li class="center" id="dl_' + totalItems
+											+ '"><span>' + deadline
+											+ '</span></li>');
+							$("#inItemText").val("");
+						}
+					}
 
-				$('#todoList').prepend(
-						'<li><input type="checkbox"/><span>' + itemText
-								+ '</span></li>');
-				$('#deadlineList')
-						.prepend(
-								'<li class="center"><span>' + deadline
-										+ '</span></li>');
-			}
+					$("#inItemText").keyup(function(e) {
+						if (e.keyCode == 13) {
+							addNewItem();
+							$("#inItemText").val("");
+						}
+					});
 
-			$("#inItemText").keyup(function(e) {
-				if (e.keyCode == 13) {
-					addNewItem();
-					$("#inItemText").val("");
-				}
-			});
-			$("#btnAdd").click(addNewItem);
-		});
+					$("#btnAdd").click(addNewItem);
+
+				});
+function updateItemStatus(x) {
+	var cbId = $(x).attr("id").replace("cb_", "");
+	var itemText = $("#item_" + cbId)
+	if ($(x).is(":checked")) {
+		itemText.css("text-decoration", "line-through");
+		itemText.css("font-weight", "600");
+		itemText.css("color", "#ccc")
+	} else {
+		itemText.css("text-decoration", "none");
+		itemText.css("font-weight", "300");
+		itemText.css("color", "#000")
+	}
+}
